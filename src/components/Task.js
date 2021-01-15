2e6d8faf037e6392a89968a51fc3af99157e0cdd
Shira Joseph <shirafjoseph@gmail.com>
2021-01-15 11:32:41 -0500

    completed data.  Test fails, though

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Task ({task: {id, title, state}, onArchiveTask, onPinTask}) {
+export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
   return (
     <div className={`list-item ${state}`}>
       <label className="checkbox">
@@ -9,32 +9,24 @@ export default function Task ({task: {id, title, state}, onArchiveTask, onPinTas
           type="checkbox"
           defaultChecked={state === 'TASK_ARCHIVED'}
           disabled={true}
-          name="checked"/>
-        <span
-          className="checkbox-custom"
-          onClick={() => onArchiveTask(id)}/>
+          name="checked"
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
       <div className="title">
-        <input
-          type="text"
-          value={title}
-          readOnly={true}
-          placeholder="Input title"/>
+        <input type="text" value={title} readOnly={true} placeholder="Input title" />
       </div>
-      <div
-        className="actions"
-        onClick={event => event.stopPropagation()}>
+
+      <div className="actions" onClick={event => event.stopPropagation()}>
         {state !== 'TASK_ARCHIVED' && (
-          // eslint-disable-next-line jsx-a11y/anchor-is-valid
           <a onClick={() => onPinTask(id)}>
-            <span className={`icon-star`}/>
+            <span className={`icon-star`} />
           </a>
         )}
       </div>
     </div>
   );
 }
-
 Task.propTypes = {
   task: PropTypes.shape({
     id: PropTypes.string.isRequired,
@@ -42,5 +34,5 @@ Task.propTypes = {
     state: PropTypes.string.isRequired,
   }),
   onArchiveTask: PropTypes.func,
-  onPinTask: PropTypes.func
-}
+  onPinTask: PropTypes.func,
+};
